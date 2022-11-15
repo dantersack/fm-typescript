d4921e91df169279d91af8fa8be8ca1d53540fa3
Dante Sack <sack_dante@hotmail.com>
2022-11-15 00:29:03 -0300

    variables and values

---
+++ typescript-fundamentals-v3/01-variables-and-values/src/index.ts
@@ -0,0 +1,20 @@
+let age: number = 29;
+
+const literalType = 6;
+
+// random time between 500 and 1000 milliseconds
+const RANDOM_WAIT_TIME = Math.round(Math.random() * 500 + 500);
+
+let startTime = new Date();
+let endTime;
+
+setTimeout(() => {
+  endTime = 0;
+  endTime = new Date();
+}, RANDOM_WAIT_TIME);
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+const result = add(2, 3);
