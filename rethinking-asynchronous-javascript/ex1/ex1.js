9691fdfa869ba97910746a56ef727681635c5f68
Dante Sack <sack_dante@hotmail.com>
2024-11-13 08:25:41 -0300

    rethinking async js - ex1 solution - callback hell

--- rethinking-asynchronous-javascript/ex1/ex1.js
+++ rethinking-asynchronous-javascript/ex1/ex1.js
@@ -1,10 +1,10 @@
 function fakeAjax(url, cb) {
-  var fake_responses = {
+  const fake_responses = {
     file1: "The first text",
     file2: "The middle text",
     file3: "The last text",
   };
-  var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
+  const randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
 
   console.log("Requesting: " + url);
 
@@ -22,8 +22,28 @@ function output(text) {
 
 function getFile(file) {
   fakeAjax(file, function (text) {
-    // what do we do here?
+    handleResponse(file, text);
   });
+}
+
+const responses = {};
+
+function handleResponse(filename, filecontent) {
+  if (!(filename in responses)) {
+    responses[filename] = filecontent;
+  }
+  const filenames = ["file1", "file2", "file3"];
+  for (let i = 0; i < filenames.length; i++) {
+    if (filenames[i] in responses) {
+      if (typeof responses[filenames[i]] === "string") {
+        output(responses[filenames[i]]);
+        responses[filenames[i]] = false;
+      }
+    } else {
+      return;
+    }
+  }
+  output("Done");
 }
 
 // request all files at once in "parallel"
